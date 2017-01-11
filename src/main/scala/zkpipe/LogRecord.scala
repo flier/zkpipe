00fa62dd2a2c3e0775eb60245b7f4fe20bdbe50e
Flier Lu <flier.lu@gmail.com>
2017-01-11 12:31:51 +0800

    Add AclPerms

--- src/main/scala/zkpipe/LogRecord.scala
+++ src/main/scala/zkpipe/LogRecord.scala
@@ -1,6 +1,6 @@
 package zkpipe
 
-import org.apache.zookeeper.ZooDefs.OpCode
+import org.apache.zookeeper.ZooDefs.{OpCode, Perms}
 import com.github.nscala_time.time.Imports._
 import com.typesafe.scalalogging.LazyLogging
 import io.prometheus.client.Counter
@@ -16,41 +16,51 @@ object LogRecord{
     val decodeBytes: Counter = Counter.build().subsystem(SUBSYSTEM).name("bytes").help("decoded bytes").register()
 }
 
+object TxnTypes extends Enumeration {
+    type TxnType = Value
+
+    val Notification = Value(OpCode.notification)
+    val Create = Value(OpCode.create)
+    val Delete = Value(OpCode.delete)
+    val Exists = Value(OpCode.exists)
+    val GetData = Value(OpCode.getData)
+    val SetData = Value(OpCode.setData)
+    val GetACL = Value(OpCode.getACL)
+    val SetACL = Value(OpCode.setACL)
+    val GetChildren = Value(OpCode.getChildren)
+    val Sync = Value(OpCode.sync)
+    val Ping = Value(OpCode.ping)
+    val GetChildren2 = Value(OpCode.getChildren2)
+    val Check = Value(OpCode.check)
+    val Multi = Value(OpCode.multi)
+    val Create2 = Value(OpCode.create2)
+    val Reconfig = Value(OpCode.reconfig)
+    val CheckWatches = Value(OpCode.checkWatches)
+    val RemoveWatches = Value(OpCode.removeWatches)
+    val CreateContainer = Value(OpCode.createContainer)
+    val DeleteContainer = Value(OpCode.deleteContainer)
+    val Auth = Value(OpCode.auth)
+    val SetWatches = Value(OpCode.setWatches)
+    val Sasl = Value(OpCode.sasl)
+    val CreateSession = Value(OpCode.createSession)
+    val CloseSession = Value(OpCode.closeSession)
+    val Error = Value(OpCode.error)
+}
+
+object AclPerms extends Enumeration {
+    type AclPerm = Value
+
+    val Read = Value(Perms.READ)
+    val Write = Value(Perms.WRITE)
+    val Create = Value(Perms.CREATE)
+    val Delete = Value(Perms.DELETE)
+    val Admin = Value(Perms.ADMIN)
+    val All = Value(Perms.ALL)
+}
+
 class LogRecord(val bytes: Array[Byte]) extends LazyLogging {
     import LogRecord._
-
-    object TxnType extends Enumeration {
-        type Type = Value
-
-        val notification = Value(OpCode.notification)
-        val create = Value(OpCode.create)
-        val delete = Value(OpCode.delete)
-        val exists = Value(OpCode.exists)
-        val getData = Value(OpCode.getData)
-        val setData = Value(OpCode.setData)
-        val getACL = Value(OpCode.getACL)
-        val setACL = Value(OpCode.setACL)
-        val getChildren = Value(OpCode.getChildren)
-        val sync = Value(OpCode.sync)
-        val ping = Value(OpCode.ping)
-        val getChildren2 = Value(OpCode.getChildren2)
-        val check = Value(OpCode.check)
-        val multi = Value(OpCode.multi)
-        val create2 = Value(OpCode.create2)
-        val reconfig = Value(OpCode.reconfig)
-        val checkWatches = Value(OpCode.checkWatches)
-        val removeWatches = Value(OpCode.removeWatches)
-        val createContainer = Value(OpCode.createContainer)
-        val deleteContainer = Value(OpCode.deleteContainer)
-        val auth = Value(OpCode.auth)
-        val setWatches = Value(OpCode.setWatches)
-        val sasl = Value(OpCode.sasl)
-        val createSession = Value(OpCode.createSession)
-        val closeSession = Value(OpCode.closeSession)
-        val error = Value(OpCode.error)
-    }
-
-    import TxnType._
+    import TxnTypes._
 
     val header: TxnHeader = new TxnHeader()
     val record: Record = SerializeUtils.deserializeTxn(bytes, header)
@@ -59,7 +69,7 @@ class LogRecord(val bytes: Array[Byte]) extends LazyLogging {
     lazy val cxid: Int = header.getCxid
     lazy val zxid: Long = header.getZxid
     lazy val time: DateTime = header.getTime.toDateTime
-    lazy val opcode: Type = apply(header.getType)
+    lazy val opcode: TxnType = apply(header.getType)
     lazy val path: Option[String] = record match {
         case r: CreateTxn  => Some(r.getPath)
         case r: CreateContainerTxn  => Some(r.getPath)
